ca49ccc44544cebd2d7fabf8cc6d13fd0d94abc6
qtipee <quent.vallat@gmail.com>
2020-02-23 15:08:22 +0100

    _base layout #16

--- technocrarc/settings.py
+++ technocrarc/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 MEDIA_URL =  'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
