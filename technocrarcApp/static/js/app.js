5aaa3d621bb867f4f6423d762cf46d54cd286e44
qtipee <quent.vallat@gmail.com>
2020-02-23 16:13:03 +0100

    Audio file Drag & Drop #16

--- technocrarcApp/static/js/app.js
+++ technocrarcApp/static/js/app.js
@@ -17,3 +17,50 @@ socket.onclose = function onClose(e) {
 if (socket.readyState == WebSocket.OPEN) {
     socket.onopen()
 }
+
+// DOM
+const $$ = {
+    soundFileInput: document.querySelector('#sound-file-input'),
+};
+
+// Drag & Drop
+// Drag enter -> can modify style in order
+document.querySelector('.input-file-dragdrop').addEventListener('dragenter', evt =>
+{
+    evt.preventDefault();
+});
+
+// Drop on file input
+document.querySelector('.input-file-dragdrop').addEventListener('drop', evt =>
+{
+    evt.preventDefault();
+    let files = evt.dataTransfer.files;
+
+    parseAudioFiles(files);
+});
+
+function parseAudioFiles(files)
+{
+    if (files.length > 1)
+    {
+        alert('You can only upload one audio file at a time.');
+    }
+    else
+    {
+        let file = files[0];
+        let fileName = file.name;
+        let fileType = file.type;
+        let fileSize = file.size;
+
+        if (! fileType.includes('audio'))
+        {
+            alert('The file format must be audio.');
+        }
+        // TODO: Can limit size here
+        else
+        {
+            $$.soundFileInput.files = files;
+            console.log('It worked !');
+        }
+    }
+}
