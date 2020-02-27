5975920af9fad1a4dc49a895f773836f624e92b7
qtipee <quent.vallat@gmail.com>
2020-02-27 15:16:40 +0100

    Upload file testing #23

--- technocrarcApp/tests.py
+++ technocrarcApp/tests.py
@@ -3,6 +3,7 @@ from django.test import Client
 from django.test import tag
 
 from django.contrib.auth.models import User
+from .models import AudioFile
 
 # Create your tests here.
 
@@ -18,7 +19,7 @@ class UserTest(TestCase):
         '''
         Inserts a user in the test database.
         '''
-        user = User.objects.create(username="test", first_name="Techno", last_name="Crarc", email="test@technocrarc.ch", password="foo")
+        User.objects.create(username="test", first_name="Techno", last_name="Crarc", email="test@technocrarc.ch", password="foo")
 
     @tag('example')
     def test_example(self):
@@ -44,3 +45,35 @@ class UserTest(TestCase):
         c = Client()
         response = c.post('/log-in', { 'username': user.username, 'password': user.password })
         self.assertEqual(200, response.status_code)
+
+class AudioFileDBTest(TestCase):
+    '''
+    Tests the insertion of an audio file in the database.
+    '''
+    def setUp(self):
+        self.user = User.objects.create(username="test")
+        AudioFile.objects.create(file='2020_27_02/test.wav', user=self.user)
+
+    @tag('audiofiledb')
+    def test_audio(self):
+        audioFile = AudioFile.objects.get(file="2020_27_02/test.wav")
+        self.assertEqual(audioFile.file, '2020_27_02/test.wav')
+        self.assertEqual(audioFile.user, self.user)
+
+class AudioFileUpload(TestCase):
+    '''
+    Tests the upload of a file.
+    '''
+    def setUp(self):
+        self.user = User.objects.create(username="test")
+        self.client = Client()
+
+    @tag('audiofileupload')
+    def test_audiofile_upload(self):
+        with open('./test/test.txt') as f:
+            self.client.post('/upload', { 'file': f })
+            AudioFile.objects.create(file=f.name, user=self.user)
+
+            audioFile = AudioFile.objects.get(file=f.name)
+            self.assertEqual(audioFile.file, f.name)
+            self.assertEqual(audioFile.user, self.user)
