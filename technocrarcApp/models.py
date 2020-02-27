99b506e209f1fb428ff58116fd48bc59e5502a05
JonasFreibur <jonas@TIC17-FREIBURGJ.intra.eiaj.ch>
2020-02-27 19:55:31 +0100

    add audio_id fk in model effectFile

--- technocrarcApp/models.py
+++ technocrarcApp/models.py
@@ -13,6 +13,7 @@ class EffectFile(models.Model):
 
     file = models.FileField(blank=False, null=False, upload_to='effects/%Y_%d_%m/')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    audio = models.ForeignKey(AudioFile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.file.name
