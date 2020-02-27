8e19d3460746c1928c34f4aca715246a0073d0f4
qtipee <quent.vallat@gmail.com>
2020-02-27 14:09:23 +0100

    Add tags #23

--- technocrarcApp/tests.py
+++ technocrarcApp/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from django.test import Client
+from django.test import tag
 
 from django.contrib.auth.models import User
 
 # Create your tests here.
 
-class UserExampleTest(TestCase):
+class UserTest(TestCase):
     '''
     No need to test the user model, because it comes from the Django Auth package.
     This test is just an example of how tests work with Django.
@@ -14,20 +15,30 @@ class UserExampleTest(TestCase):
     inherit from this class if you do not need a database to test something.
     '''
     def setUp(self):
+        '''
+        Inserts a user in the test database.
+        '''
         user = User.objects.create(username="test", first_name="Techno", last_name="Crarc", email="test@technocrarc.ch", password="foo")
 
+    @tag('example')
     def test_example(self):
+        '''
+        Example
+        '''
         user = User.objects.get(username="test")
         self.assertEqual(user.first_name, 'Techno')
         self.assertEqual(user.last_name, 'Crarc')
         self.assertEqual(user.email, 'test@technocrarc.ch')
         self.assertEqual(user.password, 'foo')
 
+    @tag('login')
+    def test_login(self):
         '''
-        Test the authenticated routes:
+        Test the authenticated routes.
         '''
+        user = User.objects.get(username="test")
 
-        # TODO: test with authenticated routes
+        # TODO: test with authenticated routes (error when not logged in -> success then)
 
         # Valid login
         c = Client()
