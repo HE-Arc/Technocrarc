49c6e8ac9166e02e5ee3cabc1790e5b5d343b6e0
qtipee <quent.vallat@gmail.com>
2020-03-21 14:27:04 +0100

    Add reverb to a sound programatically (testing) #24

--- technocrarcApp/static/js/sketch.js
+++ technocrarcApp/static/js/sketch.js
@@ -1,41 +1,71 @@
+
 var sound;
 
-function setUpAllEffects(effects) {
+function preload()
+{
+    sound = loadSound('static/media/Chiquitita.wav');
+}
+
+function test()
+{
+    let reverb = {
+        class: 'Reverb',
+        method: 'process',
+        arguments: [3, 2],
+        timestamp: 0,
+    };
+
+    setEffect(sound, reverb);
+}
+
+function playSound()
+{
+    sound.play();
+}
+
+function setup() {
+    noCanvas()
+    //fetchEffect(1)
+}
+
+// Sets the effect to the sound
+function setEffect(sound, effect)
+{
     // TODO : manage error
-    for(let effect of effects){
-        let func;
+    //
 
-        if(effect.hasOwnProperty("class"))
-        {
-            let obj = new p5[effect.class]()
-            func = () => obj[effect.method](sound, ...effect.arguments)
-        }
-        else
-        {
-            func = () => sound[effect.method](...effect.arguments)
-        }
+    let func;
 
-        sound.addCue(effect.timestamp, func)
+    // Instantiates a new p5 effect class
+    if (effect.hasOwnProperty('class'))
+    {
+        let obj = new p5[effect.class]();
+        func = () => obj[effect.method](sound, ...effect.arguments);
+    }
+    // No p5 effect class
+    else
+    {
+        func = () => sound[effect.method](...effect.arguments);
+    }
+
+    // Adds the effect to the sound at the specific timestamp
+    sound.addCue(effect.timestamp, func);
+}
+
+function setUpAllEffects(sound, effects)
+{
+    for (let effect of effects)
+    {
+        setEffect(sound, effect);
     }
 }
 
-function fetchEffect(effectId){
-
+function fetchEffect(effectId)
+{
     // fetch(`effect/${effectId}`).then((response) => {
     //     response.json().then((json) => {
     //         setUpAllEffects(json.effects)
     //         sound.play()
     //     })
     // })
-}
-
-function preload(){
-    // TODO : change sound_id
-
-    //sound = loadSound('download/36');
-}
-
-function setup() {
-    noCanvas()
-    fetchEffect(1)
 }
