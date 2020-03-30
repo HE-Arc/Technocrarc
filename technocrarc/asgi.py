fc2caee5f77dd323d9fdb5ecf2569ddeb6b09b9e
Luca Verardo <luca.verardo@pindex.ch>
2020-03-30 14:12:47 +0200

    Fixed ASGI app for WebSockets

--- technocrarc/asgi.py
+++ technocrarc/asgi.py
@@ -1,18 +1,12 @@
 """
-ASGI config for technocrarc project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
 """
 
 import os
 import django
-from channels.routing import get_default_applicatio
+from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'technocrarc.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "technocrarc.settings")
 django.setup()
-
 application = get_default_application()
