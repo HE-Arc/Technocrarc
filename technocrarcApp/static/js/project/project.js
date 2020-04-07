ad135dd81d6448997d5d3d198e0232477529e428
qtipee <quent.vallat@gmail.com>
2020-04-07 10:30:10 +0200

    Adds tooltips to tracks controls + some design fixes

--- technocrarcApp/static/js/project/project.js
+++ technocrarcApp/static/js/project/project.js
@@ -37,6 +37,7 @@ export class ProjectManager {
                         let currentID = songs[i]["id"]
                         let rowElement = document.createElement("div")
                         let colElement = document.createElement("div")
+                        let titleElement = document.createElement("div")
                         let cardPanelElement = document.createElement("div")
                         let controlsElement = document.createElement("div")
                         let infoElement = document.createElement("div")
@@ -47,26 +48,27 @@ export class ProjectManager {
                         colElement.className += "col"
                         colElement.className += " s8"
                         cardPanelElement.className += "card-panel hoverable"
+                        titleElement.className += "col s12"
                         controlsElement.className += "controls col s2"
                         infoElement.className += "controls col s2"
                         waveFormElement.id = "waveForm_" + i
 
                         let preLoader = '<div id="progressDiv' + i + '" class="progress progress-waveform"><div class="determinate" id="progress_' + i + '" style="width: 30%"></div></div>'
 
-                        controlsElement.innerHTML += '<a id="muteButton_' + i + '" class="btn-floating btn-small waves-effect waves-light deep-orange darken-1"><i class="material-icons">volume_off</i></a>'
+                        titleElement.innerHTML += '<h2 id="trackName_' + i + '" class="track-title"></h2>'
+
+                        controlsElement.innerHTML += '<a id="muteButton_' + i + '" class="btn-floating tooltipped deep-orange darken-1" data-position="top" data-tooltip="Mute this track"><i class="material-icons">volume_off</i></a>'
                         controlsElement.innerHTML += '<p class="range-field"><input type="range" id="inputVolume_' + i + '" min="0" max="100" value="100"/></p>'
-                        controlsElement.innerHTML += '<a id="isolateButton_' + i + '" class="btn-floating btn-small waves-effect waves-light deep-orange darken-1"><i class="material-icons">hearing</i></a>'
-                        controlsElement.innerHTML += '<a id="effectButton_' + i + '" class="btn-floating btn-small waves-effect waves-light deep-orange darken-1 modal-trigger" href="#effectModal"><i class="material-icons">blur_on</i></a>'
+                        controlsElement.innerHTML += '<a id="isolateButton_' + i + '" class="btn-floating tooltipped deep-orange darken-1" data-position="top" data-tooltip="Isolate this track"><i class="material-icons">hearing</i></a>'
+                        controlsElement.innerHTML += '<a id="effectButton_' + i + '" class="btn-floating tooltipped deep-orange darken-1 modal-trigger" href="#effectModal" data-position="bottom" data-tooltip="Add effect"><i class="material-icons">blur_on</i></a>'
 
-                        infoElement.innerHTML += '<p id="trackName_' + i + '"></p>'
-                        infoElement.innerHTML += '<a id="downloadButton_' + i + '" class="btn-floating btn-small waves-effect waves-light red"><i class="material-icons">file_download</i></a>'
-                        infoElement.innerHTML += '<p>Save effects</p>'
-                        infoElement.innerHTML += '<a id="saveButton_' + i + '" class="btn-floating btn-small waves-effect waves-ligth red"><i class="material-icons">save</i></a>'
-
+                        infoElement.innerHTML += '<a id="downloadButton_' + i + '" class="btn-floating tooltipped red darken-2" data-position="top" data-tooltip="Download"><i class="material-icons">file_download</i></a>'
+                        infoElement.innerHTML += '<a id="saveButton_' + i + '" class="btn-floating tooltipped red darken-2" data-position="bottom" data-tooltip="Save effects"><i class="material-icons">save</i></a>'
 
                         cardPanelElement.appendChild(waveFormElement)
                         cardPanelElement.insertAdjacentHTML('beforeend', preLoader)
                         colElement.appendChild(cardPanelElement)
+                        rowElement.appendChild(titleElement)
                         rowElement.appendChild(infoElement)
                         rowElement.appendChild(colElement)
                         rowElement.appendChild(controlsElement)
@@ -132,7 +134,9 @@ export class ProjectManager {
                         Cookies.set('discoveryTunnel', '7')
                     }
 
-
+                    // Inits tooltips
+                    var elems = document.querySelectorAll('.tooltipped');
+                    M.Tooltip.init(elems, { });
                 }
             }
             this.isEditorLocked = false
